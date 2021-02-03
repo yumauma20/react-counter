39314671d77e4dda75d20536f72972c67183f4d5
yumauma20 <71yuma10@gmail.com>
2021-02-03 19:34:32 +0900

    second

---
+++ src/components/App.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react'
+
+const App = (props) => {
+  const [count, setCount] = useState(props.count)
+  const [message, setMessage] = useState(props.message)
+
+  return (
+    <>
+      <p>
+        {" "}
+        {message} : {count}{" "}
+      </p>
+      <button onClick={() => setCount((cnt) => ++cnt)}> + </button>
+      <button onClick={() => setCount((cnt) => --cnt)}> - </button>
+      <div>
+        message : {" "}
+        <input
+          type="text"
+          value={message}
+          onChange={(e) => setMessage((msg) => (msg = e.target.value))}
+        />
+      </div>
+    </>
+  )
+}
+App.defaultProps = {
+  message : "hello",
+  count : 0,
+}
+
+export default App
